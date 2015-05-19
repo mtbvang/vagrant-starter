2fedf68f41ea9838070986d6a76ab70da4faff49
mtbvang <mtb.vang@gmail.com>
2015-05-19 17:08:49 +0200

    Added mongod provisioning and template updates.

--- provision.sh
+++ provision.sh
@@ -31,9 +31,17 @@ installConsul () {
 	consul join 172.17.42.1
 }
 
+installMongo () {
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv 7F0CEB10
+	echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
+	apt-get update && apt-get install -y mongodb-org
+	mkdir -p /data/db
+}
+
 NODE_VERSION=$(trim ${1:-0.12.2})
 CONSUL_VERSION=$(trim ${2:-0.5.0})
 
 installNode
 installConsul
+installMongo
 apt-get install -yq git
