0429e05f1de699c1a377dff0c4cb8524c267193d
mtbvang <mtb.vang@gmail.com>
2015-05-20 13:25:54 +0200

    Added post provisioning to fix mongodb bson build error.

--- post-provision.sh
+++ post-provision.sh
@@ -3,9 +3,9 @@
 apt-get install -y gcc make build-essential
 cd /vagrant
 npm install
+cp -f /vagrant/vagrant/index.js /vagrant/node_modules/mongoose/node_modules/mongodb/node_modules/bson/ext
 cd /vagrant/node_modules/mongodb/node_modules/bson/
 npm install -g node-gyp
 node-gyp rebuild
 
-cd /vagrant && npm install
 nohup mongod &
