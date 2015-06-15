49dac9719c38a22b954d81f08e0fecd7382785fc
mtbvang <mtb.vang@gmail.com>
2015-06-15 16:01:05 +0200

    Corrected path.

--- provision/ubuntu/dotfiles.sh
+++ provision/ubuntu/dotfiles.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cp /vagrant/vagrant/provision/ubuntu/bash_completion /usr/share/bash-completion/bash_completion
-cp /vagrant/vagrant/.bash_aliases ~/.bash_aliases
+cp /vagrant/vagrant/provision/ubuntu/.bash_aliases ~/.bash_aliases
